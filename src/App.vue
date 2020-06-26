de80c715313d5b26cf081155664f1dab5d0e9ce4
Diala Egwuchukwu Stephen <46764458+egdiala@users.noreply.github.com>
2020-06-26 23:17:44 +0100

    Worked on nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,111 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
+    <v-app-bar app color="primary" dark v-resize="method1">
+      <v-app-bar-nav-icon
+        @click="drawer = true"
+        color="teal"
+        v-show="menu == true"
+      ></v-app-bar-nav-icon>
+
+      <!-- Nav Drawer -->
+      <v-navigation-drawer app v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item v-for="list in lists" :key="list">
+              <v-list-item-title>{{ list }}</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+
+      <div :class="`${tClass}`">
         <v-toolbar-title>Academia</v-toolbar-title>
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer v-show="menu == false"></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <div class="text-center" v-show="menu == false">
+        <span class="ma-2" v-for="list in lists" :key="list">{{ list }}</span>
+        <v-btn small class="ma-2" tile outlined color="secondary">
+          Login
+        </v-btn>
+        <v-btn small class="ma-2" tile color="teal" dark>Sign Up</v-btn>
+        <v-btn small class="ma-2" tile large color="teal" icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </div>
+
+      <v-btn small tile large color="teal" icon v-show="menu == true">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <v-main>
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    menu: null,
+    drawer: false,
+    group: null,
+    tClass: "",
+    lists: ["Home", "School", "Courses", "About"],
   }),
+  mounted: function() {
+    this.autoTheme(); //autoTheme will execute at pageload
+    this.method1(); //method1 will execute at pageload
+  },
+  methods: {
+    //changes theme manually
+    changeState() {
+      if (
+        this.theme == "mdi-white-balance-sunny" &&
+        this.$vuetify.theme.dark == true
+      ) {
+        this.theme = "mdi-moon-waxing-crescent";
+        this.$vuetify.theme.dark = false;
+      } else {
+        this.theme = "mdi-white-balance-sunny";
+        this.$vuetify.theme.dark = true;
+      }
+    },
+    autoTheme() {
+      const hours = new Date().getHours();
+      if (hours <= 6 || hours >= 18) {
+        this.$vuetify.theme.dark = true;
+        this.theme = "mdi-white-balance-sunny";
+      } else {
+        this.$vuetify.theme.dark = false;
+        this.theme = "mdi-moon-waxing-crescent";
+      }
+    },
+    method1() {
+      var isMobile = /iPhone|iPod|Android/i.test(navigator.userAgent);
+      if (isMobile) {
+        this.menu = true;
+        this.tClass = "mx-auto";
+      } else {
+        this.menu = false;
+        this.tClass = "d-flex align-center";
+      }
+    },
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,17 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#ffffff",
+                secondary: "#1e1e1e",
+            },
+            dark: {
+                primary: "#1e1e1e",
+                secondary: "#ffffff",
+            },
+        },
+    },
+});
